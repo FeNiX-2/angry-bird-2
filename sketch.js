e68830dd4b7e137748a0c2983cdf4724babd0ea9
ManavVerma <manavverma425@gmail.com>
2020-11-24 15:43:47 +0530

    platform added

--- sketch.js
+++ sketch.js
@@ -4,26 +4,26 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
-
-function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+var bg;
+function preload(){
+    bg=loadImage("sprites/bg.png");
 }
+    
 
 function setup(){
     var canvas = createCanvas(1200,400);
+    
     engine = Engine.create();
     world = engine.world;
 
-
-    ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
-
+    
+    ground = new Ground(600,height,1200,20)
+    platform = new Ground(100,300,200,200)
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
+    pig1 = new Pig(810,350);
     log1 = new Log(810,260,300, PI/2);
-
+    
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
@@ -39,11 +39,11 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    background(bg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
+    /*console.log(box2.body.position.x);
     console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    console.log(box2.body.angle);*/
     box1.display();
     box2.display();
     ground.display();
@@ -60,5 +60,6 @@ function draw(){
     log5.display();
 
     bird.display();
+
     platform.display();
 }
